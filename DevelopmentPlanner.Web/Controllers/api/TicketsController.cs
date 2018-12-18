09a3db336ced11eee96d4ab33e1fac2447c264df
mlawrence <mlawrence@alsde.edu>
2018-12-18 15:02:21 -0600

    implement tickets api controller

---
+++ DevelopmentPlanner.Web/Controllers/api/TicketsController.cs
@@ -0,0 +1,23 @@
+﻿using DevelopmentPlanner.Web.Data;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DevelopmentPlanner.Web.Controllers.api
+{
+    public class TicketsController : ApiController
+    {
+        private PlannerContext context;
+
+        public TicketsController()
+        {
+            context = new PlannerContext();
+        }
+
+        public async Task<object> Get()
+        {
+            var list = await context.Tickets.ToListAsync();
+            return Ok(list);
+        }
+    }
+}
---
+++ DevelopmentPlanner.Web/Data/PlannerContext.cs
@@ -0,0 +1,23 @@
+﻿using DevelopmentPlanner.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentPlanner.Web.Data
+{
+    public class PlannerContext : DbContext
+    {
+        public PlannerContext() : base("PlannerContext")
+        {
+            Database.Log = msg => Debug.WriteLine(msg);
+            Database.SetInitializer<PlannerContext>(null);
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+
+}
